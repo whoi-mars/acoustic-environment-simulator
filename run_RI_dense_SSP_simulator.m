2c939897853afc717063936f3704f0c44d019b4c
Mark Goldwater <goldwater243@gmail.com>
2026-04-01 15:55:12 -0400

    Update parameters

--- run_RI_dense_SSP_simulator.m
+++ run_RI_dense_SSP_simulator.m
@@ -6,7 +6,7 @@ clc
 addpath('KRAKEN');
 addpath('utils');
 addpath('simulators');
-% addpath('/home/mark/Documents/MATLAB/')
+addpath('/home/mark/Documents/MATLAB/')
 
 % set RNG
 rng(1234)
@@ -35,17 +35,17 @@ config.NM = 8; % []
 %           Simulation Metaparameters
 %---------------------------------------------
 % adding noise to the calls
-config.ADD_NOISE = 0;
+config.ADD_NOISE = 5000;
 % number of environments to save per file
 config.ENV_CHUNK = 16;
 % number of processe in parallel pool
-config.N = 0;
+config.N = 12;
 % frequency used to find asymptotic group speed
 config.F_ASYM = 400; % [Hz]
 % If not 0, removed all simulated signals of shorter duration
 config.DURATION_FILTER = 0; % [sec]
 % noise is added to each simulated signal
-config.SNR_RANGE = [-5 5]; % [dB]
+config.SNR_RANGE = [2 5]; % [dB]
 % randomly shift simulated signal in simulation window
 config.RANDOM_SHIFT = 1;
 
@@ -60,21 +60,21 @@ config.ALPHA_V = 0.80; % []
 %---------------------------------------------
 %     Sediment Sound Speed + Attenuation
 %---------------------------------------------
-config.CSED_RANGE = [1500 1500]; % [1400 1600]; % [m/s]
-config.DELTA_CSED = 1; % 15; % [m/s]
+config.CSED_RANGE = [1400 1600]; % [m/s]
+config.DELTA_CSED = 15; % [m/s]
 config.SED_ALPHA_RANGE = [0.05 0.05]; % [dB/wavelength]
 
 %---------------------------------------------
 %             Sediment Thickness
 %---------------------------------------------
-config.H_RANGE = [8 8]; % [5 15]; % [m]
+config.H_RANGE = [5 15]; % [m]
 config.DELTA_H = 1; % [m]
 
 %---------------------------------------------
 %      Basement Sound Speed + Attenuation
 %---------------------------------------------
-config.CB_RANGE = [1800 1800]; % [1600 2200]; % [m/s]
-config.DELTA_CB = 1; % 40; % [m/s]
+config.CB_RANGE = [1600 2200]; % [m/s]
+config.DELTA_CB = 40; % [m/s]
 config.B_ALPHA_RANGE = [0.25 0.25]; % [db/wavelength]
 
 %---------------------------------------------
@@ -85,14 +85,14 @@ config.D_RANGE = [68 78]; % [m]
 %---------------------------------------------
 %              Source Depths
 %---------------------------------------------
-config.ZS_RANGE = [60 60]; % [58 62]; % [m]
+config.ZS_RANGE = [58 62]; % [m]
 config.DELTA_ZS = 1; % [m]
 
 %---------------------------------------------
 %              Source Ranges
 %---------------------------------------------
 config.R_RANGE = [4000 15000]; % [m]
-config.DELTA_R = 100; % 10; % [m]
+config.DELTA_R = 10; % 10; % [m]
 
 %------------------------------------------------------------------------
 %========================================================================
